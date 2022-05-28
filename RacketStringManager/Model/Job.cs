226c2b5b6e37e461c03f8cdf5e57011461a08750
Chrisi <>
2022-05-29 00:47:24 +0200

    added job class as core data structure

---
+++ RacketStringManager/Model/Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacketStringManager.Model
+{
+    public class Job
+    {
+        public Guid JobId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Tension { get; set; }
+
+        public string StringName { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
