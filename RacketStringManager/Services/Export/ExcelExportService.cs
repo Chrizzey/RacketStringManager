f788bfddd1a364093b1faeec38c9efb32dd3fd3d
Chrisi <>
2022-06-06 18:55:23 +0200

    added EPPlus and export service

---
+++ RacketStringManager/Services/Export/ExcelExportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RacketStringManager.Services.Export
+{
+    internal class ExcelExportService
+    {
+    }
+}
